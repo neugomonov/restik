559a7dfb114bc00531570f40fd4fccf5042acb83
Valery Neugomonov <neugomonov_v_o@protonmail.com>
2022-05-31 14:48:31 +0300

    🥺 no update?

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,11 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: "." });
 
 const customJestConfig = {
-	// testEnvironment: "jsdom",
 	clearMocks: true,
-	moduleDirectories: ["node_modules", "."],
+	moduleDirectories: ["node_modules", "<rootDir>", "src"],
 	setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
 	testRegex: "(/__tests__/.*|(\\.|/)test)\\.[jt]sx?$",
+	testEnvironment: "jest-environment-jsdom",
 };
 
 module.exports = createJestConfig(customJestConfig);
