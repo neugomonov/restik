e97480621ab1ac96defdc8862502704b2e604b43
valery <neugomonovv@gmail.com>
2022-08-20 20:51:51 +0300

    💬 comments (changed to English || deleted)

--- lib/info.ts
+++ lib/info.ts
@@ -1,58 +1,55 @@
 const info = {
-	name: 'Пиццерия',
+	name: "Пиццерия",
 	description: {
-		en: 'Your favorite pizzeria! We offer fresh and tasty food and low prices.',
-		ru: 'Ваша любимая пиццерия! Мы предлагаем свежую и вкусную еду и низкие цены.'
+		en: "Your favorite pizzeria! We offer fresh and tasty food and low prices.",
+		ru: "Ваша любимая пиццерия! Мы предлагаем свежую и вкусную еду и низкие цены.",
 	},
-	currency: '₽',
-	callingCode: '+7',
+	currency: "₽",
+	callingCode: "+7",
 
 	schedule: {
 		0: {
-			opens: '12:00',
-			closes: '22:00'
+			opens: "12:00",
+			closes: "22:00",
 		},
 		1: {
-			opens: '12:00',
-			closes: '22:00'
+			opens: "12:00",
+			closes: "22:00",
 		},
 		2: {
-			opens: '12:00',
-			closes: '22:00'
+			opens: "12:00",
+			closes: "22:00",
 		},
 		3: {
-			opens: '12:00',
-			closes: '22:00'
+			opens: "12:00",
+			closes: "22:00",
 		},
 		4: {
-			opens: '12:00',
-			closes: '22:00'
+			opens: "12:00",
+			closes: "22:00",
 		},
 		5: {
-			opens: '12:00',
-			closes: '23:00'
+			opens: "12:00",
+			closes: "23:00",
 		},
 		6: {
-			opens: '0',
-			closes: '0'
-		}
+			opens: "0",
+			closes: "0",
+		},
 	},
 	averageDelivery: 1,
-	holidays: [
-		'28/09'
-	],
+	holidays: ["28/09"],
 	types: {
 		small: {
-			en: 'Small',
-			ru: 'Немного'
+			en: "Small",
+			ru: "Немного",
 		},
 		large: {
-			en: 'Large',
-			ru: 'Много'
-		}
+			en: "Large",
+			ru: "Много",
+		},
 	},
-	// Должен ли быть включен режим разработки.
-	isDevelopment: true
+	isDevelopment: true,
 };
 
 export default info;
