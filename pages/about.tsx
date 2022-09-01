ab18aaf7ce8cc5719c56d17eca46af4435c2864f
neugomonov <neugomonovv@gmail.com>
2022-09-01 23:32:44 +0300

    🗿 persistent layout (added)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,55 +1,22 @@
 import React from "react";
-import { NextPage } from "next";
-import SidebarWithHeader from "../components/SidebarWithHeader";
-import Cart from "../components/Cart";
-import LargeWithNewsletter from "../components/Footer";
-import { Box, useColorMode, Flex } from "@chakra-ui/react";
 import { _cart } from "../lib/recoil-atoms";
 import SplitWithImage from "../components/SplitWithImage";
 import SimpleThreeColumns from "../components/SimpleThreeColumns";
 import GridListWithHeading from "../components/GridListWithHeading";
 import GridListWithCTA from "../components/GridListWithCTA";
+import { IndexLayout } from "../layouts";
 
-const Index: NextPage<unknown> = () => {
-	const { colorMode } = useColorMode();
-
+function About() {
 	return (
 		<>
-			<Flex flexDirection="column" ml={{ base: "0", md: "60" }}>
-				<Flex
-					flexDirection={{ base: "column", xl: "row" }}
-					mr={{ base: "1rem", xl: "0" }}
-				>
-					<Box
-						transition=".3s ease"
-						borderWidth="1px"
-						borderRadius="lg"
-						padding="1rem"
-						margin=".5rem"
-						width="100%"
-						mt={{ base: "6rem", md: ".5rem" }}
-						boxShadow="rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
-						backgroundColor={
-							colorMode === "dark"
-								? "rgba(6, 8, 13, 0.75)"
-								: "rgba(255, 255, 255, 0.75)"
-						}
-						position="relative"
-						backdropFilter="auto"
-						backdropBlur="20px"
-					>
-						<SplitWithImage />
-						<GridListWithHeading />
-						<GridListWithCTA />
-						<SimpleThreeColumns />
-					</Box>
-				</Flex>
-				<LargeWithNewsletter />
-			</Flex>
-			<SidebarWithHeader children />
-			<Cart />
+			<SplitWithImage />
+			<GridListWithHeading />
+			<GridListWithCTA />
+			<SimpleThreeColumns />
 		</>
 	);
-};
+}
 
-export default Index;
+About.PageLayout = IndexLayout;
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,26 @@
 import React from "react";
-import { NextPage } from "next";
-import SidebarWithHeader from "../components/SidebarWithHeader";
-import Cart from "../components/Cart";
-import LargeWithNewsletter from "../components/Footer";
 import CallToActionWithVideo from "../components/CallToActionWithVideo";
 import WithBackgroundImage from "../components/WithBackgroundImage";
 import SplitScreen from "../components/SplitScreen";
 import CallToActionWithIllustration from "../components/CallToActionWithIllustration";
 import CallToActionWithAnnotation from "../components/CallToActionWithAnnotation";
 
-import { Box, useColorMode, Flex } from "@chakra-ui/react";
-
 import { _cart } from "../lib/recoil-atoms";
 
-const Index: NextPage<unknown> = () => {
-	const { colorMode } = useColorMode();
+import { IndexLayout } from "../layouts";
 
+function Home() {
 	return (
 		<>
-			<Flex flexDirection="column" ml={{ base: "0", md: "60" }}>
-				<Flex
-					flexDirection={{ base: "column", xl: "row" }}
-					mr={{ base: "1rem", xl: "0" }}
-				>
-					<Box
-						transition=".3s ease"
-						borderWidth="1px"
-						borderRadius="lg"
-						padding="1rem"
-						margin=".5rem"
-						width="100%"
-						mt={{ base: "6rem", md: ".5rem" }}
-						boxShadow="rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
-						backgroundColor={
-							colorMode === "dark"
-								? "rgba(6, 8, 13, 0.75)"
-								: "rgba(255, 255, 255, 0.75)"
-						}
-						position="relative"
-						backdropFilter="auto"
-						backdropBlur="20px"
-					>
-						<WithBackgroundImage />
-						<CallToActionWithVideo />
-						<SplitScreen />
-						<CallToActionWithIllustration />
-						<CallToActionWithAnnotation />
-					</Box>
-				</Flex>
-				<LargeWithNewsletter />
-			</Flex>
-			<SidebarWithHeader children />
-			<Cart />
+			<WithBackgroundImage />
+			<CallToActionWithVideo />
+			<SplitScreen />
+			<CallToActionWithIllustration />
+			<CallToActionWithAnnotation />
 		</>
 	);
-};
+}
 
-export default Index;
+Home.PageLayout = IndexLayout;
+
+export default Home;
