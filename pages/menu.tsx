1b56abd1f73e2b931ef91594b7e148cf70ba206d
neugomonov <neugomonovv@gmail.com>
2022-09-06 23:50:27 +0300

    🔨 refactoring (unused imports)

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import MenuBox from "../components/MenuBox";
 import { WithSideContentLayout } from "../layouts/menu";
 import { _cart } from "../lib/recoil-atoms";
