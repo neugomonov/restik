11db8141c969192d2aabea2291ef22266e3b1bf6
neugomonov <neugomonovv@gmail.com>
2022-09-07 00:04:28 +0300

    🔨 refactoring

--- utils/merge.ts
+++ utils/merge.ts
@@ -1,12 +1,21 @@
-type Item = {name: string; type: string; price: number; quantity: number};
+type Item = { name: string; type: string; price: number; quantity: number };
 
 export const merge = (items: Item[], item: Item) => {
-	const same = items.find(element => element.name === item.name && element.type === item.type);
+	const same = items.find(
+		(element) => element.name === item.name && element.type === item.type
+	);
 
 	if (same) {
 		return [
-			...items.filter(element => (element.name !== item.name) || (element.type !== item.type)),
-			{name: item.name, type: item.type, price: item.price, quantity: same.quantity + item.quantity}
+			...items.filter(
+				(element) => element.name !== item.name || element.type !== item.type
+			),
+			{
+				name: item.name,
+				type: item.type,
+				price: item.price,
+				quantity: same.quantity + item.quantity,
+			},
 		];
 	}
 
