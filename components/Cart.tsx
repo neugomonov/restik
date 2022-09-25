3ebeda34a3e0f6114e133ca75a5b3e5a43441186
neugomonov <neugomonovv@gmail.com>
2022-09-25 14:48:28 +0300

    🔨 refactoring (unused imports & declarations)

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useRef } from "react";
-import stringifyCartPositions from "./stringifyCartPositions";
 
 import {
 	IconButton,
@@ -10,7 +9,6 @@ import {
 	Text,
 	useColorMode,
 	useDisclosure,
-	useToast,
 } from "@chakra-ui/react";
 
 import useTranslation from "next-translate/useTranslation";
@@ -23,14 +21,8 @@ import CartDrawerBody from "./CartDrawerBody";
 import CartDrawerFooter from "./CartDrawerFooter";
 
 const Drawer = dynamic(async () => (await import("@chakra-ui/react")).Drawer);
-const DrawerBody = dynamic(
-	async () => (await import("@chakra-ui/react")).DrawerBody
-);
 const DrawerHeader = dynamic(
 	async () => (await import("@chakra-ui/react")).DrawerHeader
-);
-const DrawerFooter = dynamic(
-	async () => (await import("@chakra-ui/react")).DrawerFooter
 );
 const DrawerOverlay = dynamic(
 	async () => (await import("@chakra-ui/react")).DrawerOverlay
@@ -41,40 +33,6 @@ const DrawerContent = dynamic(
 const DrawerCloseButton = dynamic(
 	async () => (await import("@chakra-ui/react")).DrawerCloseButton
 );
-const Stat = dynamic(async () => (await import("@chakra-ui/react")).Stat);
-const StatLabel = dynamic(
-	async () => (await import("@chakra-ui/react")).StatLabel
-);
-const StatNumber = dynamic(
-	async () => (await import("@chakra-ui/react")).StatNumber
-);
-const StatHelpText = dynamic(
-	async () => (await import("@chakra-ui/react")).StatHelpText
-);
-const AlertDialog = dynamic(
-	async () => (await import("@chakra-ui/react")).AlertDialog
-);
-const AlertDialogBody = dynamic(
-	async () => (await import("@chakra-ui/react")).AlertDialogBody
-);
-const AlertDialogHeader = dynamic(
-	async () => (await import("@chakra-ui/react")).AlertDialogHeader
-);
-const AlertDialogFooter = dynamic(
-	async () => (await import("@chakra-ui/react")).AlertDialogFooter
-);
-const AlertDialogContent = dynamic(
-	async () => (await import("@chakra-ui/react")).AlertDialogContent
-);
-const AlertDialogOverlay = dynamic(
-	async () => (await import("@chakra-ui/react")).AlertDialogOverlay
-);
-interface CartPosition {
-	quantity: string;
-	name: string;
-	type: string;
-	price: string;
-}
 
 export default function MenuBox() {
 	const router = useRouter();
@@ -82,7 +40,6 @@ export default function MenuBox() {
 
 	const [cart, setCart] = useRecoilState(_cart);
 	const { colorMode } = useColorMode();
-	const toast = useToast();
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const btnRef = useRef();
 	const {
@@ -90,76 +47,9 @@ export default function MenuBox() {
 		onOpen: onAlertOpen,
 		onClose: onAlertClose,
 	} = useDisclosure();
-	const cancelRef = useRef();
 	const { t, lang } = useTranslation("common");
 	const items = cart.items.map((x) => x.quantity).reduce((a, b) => a + b, 0);
-	const handleNew = stringifyCartPositions();
-	const handleClick = (route: string) => {
-		return async () => {
-			await router.push(route, route, {
-				locale: "ru",
-			});
-		};
-	};
-	const handleRemovePositionClick = (item: {
-		quantity: number;
-		name: string;
-		type: string;
-		price: number;
-	}) => {
-		return () => {
-			if (item.quantity === 1) {
-				setCart((previous) => ({
-					items: previous.items.filter(
-						(element) =>
-							element.name !== item.name || element.type !== item.type
-					),
-					total: previous.total - item.price,
-				}));
-			} else {
-				setCart((previous) => ({
-					items: [
-						...previous.items.filter(
-							(element) =>
-								element.name !== item.name || element.type !== item.type
-						),
-						{
-							name: item.name,
-							type: item.type,
-							price: item.price,
-							quantity: item.quantity - 1,
-						},
-					],
-					total: previous.total - item.price,
-				}));
-			}
-		};
-	};
 
-	const handleAddPositionClick = (item: {
-		quantity: number;
-		name: string;
-		type: string;
-		price: number;
-	}) => {
-		return () => {
-			setCart((previous) => ({
-				items: [
-					...previous.items.filter(
-						(element) =>
-							element.name !== item.name || element.type !== item.type
-					),
-					{
-						name: item.name,
-						type: item.type,
-						price: item.price,
-						quantity: item.quantity + 1,
-					},
-				],
-				total: previous.total + item.price,
-			}));
-		};
-	};
 	// 🔨 There are other anonymous functions in the tree that need refactoring too, I'll deal with them later. Later...
 	return (
 		<>
