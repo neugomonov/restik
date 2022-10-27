06f4e88d6d23a43228cc30d00f3ded846130e020
neugomonov <neugomonovv@gmail.com>
2022-10-27 22:49:04 +0300

    🍕 pizza proportions in sidebar (fixed)

--- components/pizza-loader.tsx
+++ components/pizza-loader.tsx
@@ -21,7 +21,7 @@ export const PizzaContainer = forwardRef<HTMLDivElement>(
 			mt={["-20px", "-60px", "-120px"]}
 			mb={["-40px", "-140px", "-200px"]}
 			w={[280, 480, 640]}
-			h={[280, 480, 640]}
+			h={[280, 340, 280]}
 			position="relative"
 		>
 			{children}
