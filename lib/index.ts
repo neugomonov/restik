9faa10b1e8b7b61540ad6bb512ea29f26de7d517
neugomonov <neugomonovv@gmail.com>
2022-11-04 23:39:09 +0300

    👅 i18n (wip)

--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,33 @@ const index = {
 		en: "Ask questions in the chat with Pizzeria, and we'll answer right away!",
 		ru: "Задавайте вопросы в чат с Пиццерией, и мы обязательно ответим!",
 	},
+	WithBackgroundImage: {
+		en: "We prepare delicious pizza with the best traditions of Michelin-rated chefs. Try it!",
+		ru: "Готовим вкуснейшую пиццу по лучшим традициям поваров рейтинга Мишлена. Попробуйте!",
+	},
+	WithBackgroundImageButton: {
+		en: "Pizzas! 🍕",
+		ru: "К пиццам! 🍕",
+	},
+	CallToActionWithVideo1: { en: "We use", ru: "Готовим из" },
+	CallToActionWithVideo2: {
+		en: "the best ingredients",
+		ru: "лучших ингредиентов",
+	},
+	CallToActionWithVideo3: {
+		en: "High quality is what our pizzeria puts in the first place. We choose the best products, the best equipment and the best specialists! The automation of many production processes allows us to keep the quality always at the same high level in all our branches. So, for example, we use dough presses, with the help of which an ideal and uniform pizza thickness is achieved. And the conveyor ovens that are in our kitchen allow the chef not to worry about the quality of baking - automation is fully responsible for this. The result is always delicious pizza, no burnt edges or undercooked core!",
+		ru: "Высокое качество - вот что наша пиццерия ставит на первое место. Мы выбираем лучшие продукты, лучшее оборудование и лучших специалистов!	Держать качество всегда на одинаково высоком уровне во всех наших	филиалах нам позволяет автоматизация многих процессов производства.	Так, например, мы используем тестопрессы, при помощи которых	достигается идеальная и равномерная толщина пиццы. А конвейерные	печи, которые стоят на нашей кухне, позволяют повару не беспокоиться	за качество пропекания - за это полностью отвечает автоматика.	Результат - всегда вкусная пицца, никаких подгоревших краев или непропеченной сердцевины!",
+	},
+	CallToActionWithVideoButton1: { en: "See the menu", ru: "Посмотреть меню" },
+	CallToActionWithVideoButton2: { en: "How do we cook?", ru: "Как мы готовим" },
+	SplitScreen1: { en: "We are", ru: "Мы во всех" },
+	SplitScreen2: { en: "everywhere", ru: "соцсетях" },
+	SplitScreen3: {
+		en: "We place our new promotions there. Subscribe and stay up to date with all the profitable offers of our pizzeria!",
+		ru: "Там мы размещаем наши новые акции. Подписывайтесь и будьте в курсе	всех выгодных предложений нашей пиццерии!",
+	},
+	SplitScreenButton1: { en: "See our promos", ru: "Посмотреть наши акции" },
+	SplitScreenButton2: { en: "Our Telegram", ru: "Наш Телеграм" },
 };
 
 export default index;
