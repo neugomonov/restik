295d44a9712f9f84e1cbf66a29c868160a9aca24
neugomonov <neugomonovv@gmail.com>
2022-11-07 23:14:46 +0300

    🤪 framer-motion stuff

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -115,7 +115,29 @@ function Profile() {
 											</Text>
 											<ProfileButtons />{" "}
 										</VStack>
-										<Box display={{ base: "none", md: "flex" }}>
+										<Box
+											display={{ base: "none", md: "flex" }}
+											as={motion.div}
+											cursor="pointer"
+											drag
+											dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+											whileDrag={{ scale: 1.2 }}
+											dragTransition={{
+												bounceStiffness: 1399,
+												bounceDamping: 10,
+											}}
+											whileTap={{
+												scale: 0.9,
+											}}
+											whileHover={{
+												scale: 1.2,
+												transition: {
+													type: "spring",
+													bounce: 0.8,
+													duration: 1,
+												},
+											}}
+										>
 											<FiChevronDown />
 										</Box>
 									</VStack>
