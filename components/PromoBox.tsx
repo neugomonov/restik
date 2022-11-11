432f56e2b0b921ac57cad69e4d8a7a3426bb5712
neugomonov <neugomonovv@gmail.com>
2022-11-11 16:28:42 +0300

    ✨ blur toggle (components migrated to Recoil)

--- components/PromoBox.tsx
+++ components/PromoBox.tsx
@@ -15,12 +15,14 @@ import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 import { IoRestaurantOutline } from "react-icons/io5";
+import { useRecoilState } from "recoil";
 import info from "../lib/info";
 import promo from "../lib/promo";
+import { _blur } from "../lib/recoil-atoms";
 import { BlurContext } from "./BlurContext";
 
 export default function PromoBox() {
-	const { t, lang } = useTranslation("home");
+	const { t, lang } = useTranslation("common");
 
 	const router = useRouter();
 	const { colorMode } = useColorMode();
@@ -29,8 +31,7 @@ export default function PromoBox() {
 			await router.push(route, route);
 		};
 	};
-	// @ts-expect-error
-	const { blurMode } = useContext(BlurContext);
+	const [blurMode, setBlurMode] = useRecoilState(_blur);
 
 	return (
 		<Box
@@ -47,7 +48,7 @@ export default function PromoBox() {
 			}
 			position="sticky"
 			top="100%"
-			backdropFilter={blurMode ? "auto" : "none"}
+			backdropFilter={blurMode.blur ? "auto" : "none"}
 			backdropBlur="20px"
 		>
 			<div
