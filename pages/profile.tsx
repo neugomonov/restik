eb4f9f75cf8b4a489e27bc541801632ff26d1773
neugomonov <neugomonovv@gmail.com>
2022-11-12 23:17:24 +0300

    🔨 refactoring (conditional profile skeleton added)

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -17,6 +17,8 @@ import { useSession } from "next-auth/react";
 import useTranslation from "next-translate/useTranslation";
 import { CgProfile } from "react-icons/cg";
 import { FiChevronDown } from "react-icons/fi";
+import LoginHeader from "../components/LoginHeader";
+import LoginSidebar from "../components/LoginSidebar";
 import OrdersTable from "../components/OrdersTable";
 import ProfileButtons from "../components/ProfileButtons";
 import { WithSideContentLayout } from "../layouts/menu";
@@ -86,62 +88,70 @@ function Profile() {
 				>
 					<Stack direction={{ base: "column-reverse", lg: "row" }} spacing={3}>
 						<Box padding="1rem" width="100%" minW={{ base: "auto", xl: "50%" }}>
-							{" "}
 							<Flex alignItems={"center"} justifyContent={"center"} mx="2">
-								<Box
-									p={4}
-									alignItems={"center"}
-									justifyContent={"center"}
-									transition=".3s ease"
-									borderWidth="1px"
-									borderRadius="lg"
-									boxShadow="rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
-									backgroundColor={
-										colorMode === "dark"
-											? "rgba(6, 8, 13, 0.25)"
-											: "rgba(255, 255, 255, 0.25)"
-									}
-									position="relative"
-								>
-									<VStack>
-										<Avatar size={"3xl"} src={session?.user?.image!} />
-										<VStack alignItems="center" spacing="0" ml="2">
-											<Text fontSize="3xl">{session?.user?.name}</Text>
-											<Text
-												fontSize="sm"
-												color={useColorModeValue("gray.600", "gray.300")}
+								{session ? (
+									<Box
+										p={4}
+										alignItems={"center"}
+										justifyContent={"center"}
+										transition=".3s ease"
+										borderWidth="1px"
+										borderRadius="lg"
+										boxShadow="rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
+										backgroundColor={
+											colorMode === "dark"
+												? "rgba(6, 8, 13, 0.25)"
+												: "rgba(255, 255, 255, 0.25)"
+										}
+										position="relative"
+									>
+										<VStack>
+											<Avatar size={"3xl"} src={session.user?.image!} />
+											<VStack alignItems="center" spacing="0" ml="2">
+												<Text fontSize="3xl">{session.user?.name}</Text>
+												<Text
+													fontSize="sm"
+													color={useColorModeValue("gray.600", "gray.300")}
+												>
+													{session.user?.email!}
+												</Text>
+												<ProfileButtons />{" "}
+											</VStack>
+											<Box
+												display={{ base: "none", md: "flex" }}
+												as={motion.div}
+												cursor="pointer"
+												drag
+												dragConstraints={{
+													top: 0,
+													left: 0,
+													right: 0,
+													bottom: 0,
+												}}
+												whileDrag={{ scale: 1.2 }}
+												dragTransition={{
+													bounceStiffness: 1399,
+													bounceDamping: 10,
+												}}
+												whileTap={{
+													scale: 0.9,
+												}}
+												whileHover={{
+													scale: 1.2,
+													transition: {
+														type: "spring",
+														bounce: 0.8,
+														duration: 1,
+													},
+												}}
 											>
-												{session?.user?.email!}
-											</Text>
-											<ProfileButtons />{" "}
+												<FiChevronDown />
+											</Box>
 										</VStack>
-										<Box
-											display={{ base: "none", md: "flex" }}
-											as={motion.div}
-											cursor="pointer"
-											drag
-											dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
-											whileDrag={{ scale: 1.2 }}
-											dragTransition={{
-												bounceStiffness: 1399,
-												bounceDamping: 10,
-											}}
-											whileTap={{
-												scale: 0.9,
-											}}
-											whileHover={{
-												scale: 1.2,
-												transition: {
-													type: "spring",
-													bounce: 0.8,
-													duration: 1,
-												},
-											}}
-										>
-											<FiChevronDown />
-										</Box>
-									</VStack>
-								</Box>
+									</Box>
+								) : (
+									<LoginHeader />
+								)}
 							</Flex>
 						</Box>
 						<Stack
@@ -166,21 +176,25 @@ function Profile() {
 						</Stack>
 					</Stack>
 				</Stack>
-				<Stack
-					alignItems={"center"}
-					justifyContent={"center"}
-					direction="column"
-				>
-					<Box
-						borderWidth="1px"
-						borderRadius="lg"
-						padding="1rem"
-						width="100%"
-						minW={{ base: "auto", xl: "50%" }}
+				{session ? (
+					<Stack
+						alignItems={"center"}
+						justifyContent={"center"}
+						direction="column"
 					>
-						<OrdersTable />
-					</Box>
-				</Stack>
+						<Box
+							borderWidth="1px"
+							borderRadius="lg"
+							padding="1rem"
+							width="100%"
+							minW={{ base: "auto", xl: "50%" }}
+						>
+							<OrdersTable />
+						</Box>
+					</Stack>
+				) : (
+					<></>
+				)}{" "}
 			</Stack>
 		</>
 	);
