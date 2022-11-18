de14667c4d7d43285d2770e65fb654a2cf28b64b
neugomonov <neugomonovv@gmail.com>
2022-11-18 23:59:37 +0300

    🔨 wrappers

--- pages/news.tsx
+++ pages/news.tsx
@@ -9,6 +9,7 @@ import {
 } from "@chakra-ui/react";
 import useTranslation from "next-translate/useTranslation";
 import { BiNews } from "react-icons/bi";
+import MenuContentChakraWrapper from "../components/main/MenuContentChakraWrapper";
 import { ProductImage } from "../components/MenuBox";
 import MotionTag from "../components/motion/MotionTag";
 import MotionTopIconBox from "../components/motion/MotionTopIconBox";
@@ -21,62 +22,64 @@ function News() {
 	// TODO: figure out the way to not to re-render the secondary component when not needed
 	return (
 		<>
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "space-between",
-				}}
-			>
-				{info.isDevelopment && (
-					<MotionTag>{info.news[lang as "en" | "ru"]}</MotionTag>
-				)}{" "}
-				<MotionTopIconBox>
-					<IconButton aria-label="News" icon={<BiNews />} />
-				</MotionTopIconBox>
-			</div>
-			<Stack spacing={5}>
-				<SimpleGrid
-					minChildWidth="15rem"
-					spacing={3}
-					justifyContent="center"
-					alignItems="center"
-					pt="1rem"
+			<MenuContentChakraWrapper>
+				<div
+					style={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "space-between",
+					}}
 				>
-					{news(lang as "en" | "ru").map((item) => (
-						<Link key={item.name}>
-							<Box borderWidth="1px" borderRadius="lg" padding="1rem">
-								<Stack spacing={3}>
-									<Box height="10rem" position="relative">
-										<ProductImage
-											src={`/${item.image}`}
-											alt={`${t("photoOf")} ${item.name}`}
-											draggable={false}
-											loading="lazy"
-											decoding="async"
-											// @ts-expect-error - Type 'true' is not assignable to type 'ResponsiveValue<Union<Color | "current" | ... 176 more ... | "chakra-placeholder-color">> | undefined'.ts(2322)
-											fill
-											sizes="(max-width: 768px) 100vw,
+					{info.isDevelopment && (
+						<MotionTag>{info.news[lang as "en" | "ru"]}</MotionTag>
+					)}{" "}
+					<MotionTopIconBox>
+						<IconButton aria-label="News" icon={<BiNews />} />
+					</MotionTopIconBox>
+				</div>
+				<Stack spacing={5}>
+					<SimpleGrid
+						minChildWidth="15rem"
+						spacing={3}
+						justifyContent="center"
+						alignItems="center"
+						pt="1rem"
+					>
+						{news(lang as "en" | "ru").map((item) => (
+							<Link key={item.name}>
+								<Box borderWidth="1px" borderRadius="lg" padding="1rem">
+									<Stack spacing={3}>
+										<Box height="10rem" position="relative">
+											<ProductImage
+												src={`/${item.image}`}
+												alt={`${t("photoOf")} ${item.name}`}
+												draggable={false}
+												loading="lazy"
+												decoding="async"
+												// @ts-expect-error - Type 'true' is not assignable to type 'ResponsiveValue<Union<Color | "current" | ... 176 more ... | "chakra-placeholder-color">> | undefined'.ts(2322)
+												fill
+												sizes="(max-width: 768px) 100vw,
 											(max-width: 1200px) 50vw,
 											33vw"
-											objectFit="cover"
-											borderRadius="md"
-										/>
-									</Box>
-									<Text colorScheme={"gray"}>{item.date}</Text>
+												objectFit="cover"
+												borderRadius="md"
+											/>
+										</Box>
+										<Text colorScheme={"gray"}>{item.date}</Text>
 
-									<Heading size="md" mr="1%">
-										{item.name}
-									</Heading>
-									<Text colorScheme={"gray"} fontSize=".8rem">
-										{item.ingredients.join(", ")}
-									</Text>
-								</Stack>
-							</Box>
-						</Link>
-					))}
-				</SimpleGrid>
-			</Stack>
+										<Heading size="md" mr="1%">
+											{item.name}
+										</Heading>
+										<Text colorScheme={"gray"} fontSize=".8rem">
+											{item.ingredients.join(", ")}
+										</Text>
+									</Stack>
+								</Box>
+							</Link>
+						))}
+					</SimpleGrid>
+				</Stack>
+			</MenuContentChakraWrapper>
 		</>
 	);
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -17,8 +17,9 @@ import useTranslation from "next-translate/useTranslation";
 import { CgProfile } from "react-icons/cg";
 import { FiChevronDown } from "react-icons/fi";
 import LoginHeader from "../components/LoginHeader";
-import MotionTopIconBox from "../components/motion/MotionTopIconBox";
+import MenuContentChakraWrapper from "../components/main/MenuContentChakraWrapper";
 import MotionTag from "../components/motion/MotionTag";
+import MotionTopIconBox from "../components/motion/MotionTopIconBox";
 import OrdersTable from "../components/OrdersTable";
 import ProfileButtons from "../components/ProfileButtons";
 import { WithSideContentLayout } from "../layouts/menu";
@@ -32,137 +33,146 @@ function Profile() {
 	// TODO: there is a problem with the gif on narrow devices. Gotta fix.
 	return (
 		<>
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "space-between",
-				}}
-			>
-				{info.isDevelopment && <MotionTag>{t("profile")}</MotionTag>}{" "}
-				<MotionTopIconBox>
-					<IconButton aria-label="Profile" icon={<CgProfile />} />
-				</MotionTopIconBox>
-			</div>
-			<Stack spacing={5}>
-				<Stack
-					minW={{ base: "auto", xl: "20rem" }}
-					spacing={3}
-					px={{ base: "1rem", xl: "10%" }}
-					direction={"row"}
+			<MenuContentChakraWrapper>
+				<div
+					style={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "space-between",
+					}}
 				>
-					<Stack direction={{ base: "column-reverse", lg: "row" }} spacing={3}>
-						<Box padding="1rem" width="100%" minW={{ base: "auto", xl: "50%" }}>
-							<Flex alignItems={"center"} justifyContent={"center"} mx="2">
-								{session ? (
-									<Box
-										p={4}
-										alignItems={"center"}
-										justifyContent={"center"}
-										transition=".3s ease"
-										borderWidth="1px"
-										borderRadius="lg"
-										boxShadow="rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
-										backgroundColor={
-											colorMode === "dark"
-												? "rgba(6, 8, 13, 0.25)"
-												: "rgba(255, 255, 255, 0.25)"
-										}
-										position="relative"
-									>
-										<VStack>
-											{/* eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain */}
-											<Avatar size={"3xl"} src={session.user?.image!} />
-											<VStack alignItems="center" spacing="0" ml="2">
-												<Text fontSize="3xl">{session.user?.name}</Text>
-												<Text
-													fontSize="sm"
-													color={useColorModeValue("gray.600", "gray.300")}
-												>
-													{/* eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain */}
-													{session.user?.email!}
-												</Text>
-												<ProfileButtons />{" "}
-											</VStack>
-											<Box
-												display={{ base: "none", md: "flex" }}
-												as={motion.div}
-												cursor="pointer"
-												drag
-												dragConstraints={{
-													top: 0,
-													left: 0,
-													right: 0,
-													bottom: 0,
-												}}
-												whileDrag={{ scale: 1.2 }}
-												dragTransition={{
-													bounceStiffness: 1399,
-													bounceDamping: 10,
-												}}
-												whileTap={{
-													scale: 0.9,
-												}}
-												whileHover={{
-													scale: 1.2,
-													transition: {
-														type: "spring",
-														bounce: 0.8,
-														duration: 1,
-													},
-												}}
-											>
-												<FiChevronDown />
-											</Box>
-										</VStack>
-									</Box>
-								) : (
-									<LoginHeader />
-								)}
-							</Flex>
-						</Box>
+					{info.isDevelopment && <MotionTag>{t("profile")}</MotionTag>}{" "}
+					<MotionTopIconBox>
+						<IconButton aria-label="Profile" icon={<CgProfile />} />
+					</MotionTopIconBox>
+				</div>
+				<Stack spacing={5}>
+					<Stack
+						minW={{ base: "auto", xl: "20rem" }}
+						spacing={3}
+						px={{ base: "1rem", xl: "10%" }}
+						direction={"row"}
+					>
 						<Stack
-							direction="column"
-							pl={{ base: "none", xl: "10%" }}
-							spacing={5}
+							direction={{ base: "column-reverse", lg: "row" }}
+							spacing={3}
 						>
-							<Heading size="lg">
-								{index.profileMessage[lang as "en" | "ru"]}
-							</Heading>
-							<Image
-								src="https://images.squarespace-cdn.com/content/v1/529fb134e4b0dbf53fa8fa91/1519937177934-Y9HTRD1O0HTVDJECU42X/02_.gif"
-								alt="profile card gif"
-								draggable={false}
-								loading="lazy"
-								decoding="async"
-								width="auto"
-								height={300}
-								objectFit="cover"
-								borderRadius="md"
-							/>
+							<Box
+								padding="1rem"
+								width="100%"
+								minW={{ base: "auto", xl: "50%" }}
+							>
+								<Flex alignItems={"center"} justifyContent={"center"} mx="2">
+									{session ? (
+										<Box
+											p={4}
+											alignItems={"center"}
+											justifyContent={"center"}
+											transition=".3s ease"
+											borderWidth="1px"
+											borderRadius="lg"
+											boxShadow="rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
+											backgroundColor={
+												colorMode === "dark"
+													? "rgba(6, 8, 13, 0.25)"
+													: "rgba(255, 255, 255, 0.25)"
+											}
+											position="relative"
+										>
+											<VStack>
+												{/* eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain */}
+												<Avatar size={"3xl"} src={session.user?.image!} />
+												<VStack alignItems="center" spacing="0" ml="2">
+													<Text fontSize="3xl">{session.user?.name}</Text>
+													<Text
+														fontSize="sm"
+														color={useColorModeValue("gray.600", "gray.300")}
+													>
+														{/* eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain */}
+														{session.user?.email!}
+													</Text>
+													<ProfileButtons />{" "}
+												</VStack>
+												<Box
+													display={{ base: "none", md: "flex" }}
+													as={motion.div}
+													cursor="pointer"
+													drag
+													dragConstraints={{
+														top: 0,
+														left: 0,
+														right: 0,
+														bottom: 0,
+													}}
+													whileDrag={{ scale: 1.2 }}
+													dragTransition={{
+														bounceStiffness: 1399,
+														bounceDamping: 10,
+													}}
+													whileTap={{
+														scale: 0.9,
+													}}
+													whileHover={{
+														scale: 1.2,
+														transition: {
+															type: "spring",
+															bounce: 0.8,
+															duration: 1,
+														},
+													}}
+												>
+													<FiChevronDown />
+												</Box>
+											</VStack>
+										</Box>
+									) : (
+										<LoginHeader />
+									)}
+								</Flex>
+							</Box>
+							<Stack
+								direction="column"
+								pl={{ base: "none", xl: "10%" }}
+								spacing={5}
+							>
+								<Heading size="lg">
+									{index.profileMessage[lang as "en" | "ru"]}
+								</Heading>
+								<Image
+									src="https://images.squarespace-cdn.com/content/v1/529fb134e4b0dbf53fa8fa91/1519937177934-Y9HTRD1O0HTVDJECU42X/02_.gif"
+									alt="profile card gif"
+									draggable={false}
+									loading="lazy"
+									decoding="async"
+									width="auto"
+									height={300}
+									objectFit="cover"
+									borderRadius="md"
+								/>
+							</Stack>
 						</Stack>
 					</Stack>
-				</Stack>
-				{session ? (
-					<Stack
-						alignItems={"center"}
-						justifyContent={"center"}
-						direction="column"
-					>
-						<Box
-							borderWidth="1px"
-							borderRadius="lg"
-							padding="1rem"
-							width="100%"
-							minW={{ base: "auto", xl: "50%" }}
+					{session ? (
+						<Stack
+							alignItems={"center"}
+							justifyContent={"center"}
+							direction="column"
 						>
-							<OrdersTable />
-						</Box>
-					</Stack>
-				) : (
-					<></>
-				)}{" "}
-			</Stack>
+							<Box
+								borderWidth="1px"
+								borderRadius="lg"
+								padding="1rem"
+								width="100%"
+								minW={{ base: "auto", xl: "50%" }}
+							>
+								<OrdersTable />
+							</Box>
+						</Stack>
+					) : (
+						<></>
+					)}{" "}
+				</Stack>
+			</MenuContentChakraWrapper>
 		</>
 	);
 }
