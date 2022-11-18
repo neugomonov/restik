4a8be15860ba3112f324b0224bb69b8096f13b71
neugomonov <neugomonovv@gmail.com>
2022-11-18 23:59:09 +0300

    🔻 framer-motion (presence animations)

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,10 +1,24 @@
+import { motion } from "framer-motion";
+import MenuContentChakraWrapper from "../components/main/MenuContentChakraWrapper";
 import MenuBox from "../components/MenuBox";
 import { WithSideContentLayout } from "../layouts/menu";
-
 function Menu() {
 	return (
 		<>
-			<MenuBox />
+			<motion.div
+				exit={{ y: -100, scale: 0.9 }}
+				initial={{ y: 1000, scale: 1.1 }}
+				animate={{
+					y: 0,
+					scale: 1,
+					transition: { type: "spring", bounce: 0.3, duration: 0.4 },
+				}}
+				key={"2"}
+			>
+				<MenuContentChakraWrapper>
+					<MenuBox />
+				</MenuContentChakraWrapper>
+			</motion.div>
 		</>
 	);
 }
