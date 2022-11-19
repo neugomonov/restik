73615a3598b2c2fc19287b32875078ffe58c6c0a
neugomonov <neugomonovv@gmail.com>
2022-11-19 03:42:11 +0300

    🔨 chakra wrapper (applied)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import GridListWithCTA from "../components/GridListWithCTA";
 import GridListWithHeading from "../components/GridListWithHeading";
+import MainContentChakraWrapper from "../components/main/MainContentChakraWrapper";
 import MenuContentMotionWrapper from "../components/motion/MenuContentMotionWrapper";
 import SimpleThreeColumns from "../components/SimpleThreeColumns";
 import SplitWithImage from "../components/SplitWithImage";
@@ -9,10 +10,12 @@ function About() {
 	return (
 		<>
 			<MenuContentMotionWrapper>
+				<MainContentChakraWrapper>
 					<SplitWithImage />
 					<GridListWithHeading />
 					<GridListWithCTA />
 					<SimpleThreeColumns />
+				</MainContentChakraWrapper>
 			</MenuContentMotionWrapper>
 		</>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import CallToActionWithAnnotation from "../components/CallToActionWithAnnotation";
 import CallToActionWithIllustration from "../components/CallToActionWithIllustration";
 import CallToActionWithVideo from "../components/CallToActionWithVideo";
+import MainContentChakraWrapper from "../components/main/MainContentChakraWrapper";
 import MenuContentMotionWrapper from "../components/motion/MenuContentMotionWrapper";
 import SplitScreen from "../components/SplitScreen";
 import WithBackgroundImage from "../components/WithBackgroundImage";
@@ -10,11 +11,13 @@ function Home() {
 	return (
 		<>
 			<MenuContentMotionWrapper>
+				<MainContentChakraWrapper>
 					<WithBackgroundImage />
 					<CallToActionWithVideo />
 					<SplitScreen />
 					<CallToActionWithIllustration />
 					<CallToActionWithAnnotation />
+				</MainContentChakraWrapper>
 			</MenuContentMotionWrapper>
 		</>
 	);
