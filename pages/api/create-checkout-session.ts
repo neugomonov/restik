d459ae333747abdffa446406f0d6769ac24fb994
neugomonov <neugomonovv@gmail.com>
2022-11-23 10:04:44 +0300

    🤔 create-checkout-session (typescriptified)

---
+++ pages/api/create-checkout-session.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export interface IGetUserAuthInfoRequest extends Request {
+	items: {
+		name: string;
+		type: string;
+		price: number;
+		quantity: number;
+	}[];
+	email: string;
+	phone: string;
+}
+
+export default async (req: Request, res: Response) => {
+	const { items, email, phone } = req.body;
+	const transformedItems = items.map(
+		(item: {
+			quantity: number;
+			name: string;
+			price: number;
+			image: string;
+		}) => ({
+			quantity: item.quantity,
+			price_data: {
+				currency: "rub",
+				unit_amount: item.price * 100,
+				product_data: {
+					name: item.name,
+					images: [item.image],
+				},
+			},
+		})
+	);
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ["card"],
+		shipping_rates: ["shr_1LC4U2FxzVucXSOXI4V070yv"],
+		shipping_address_collection: {
+			allowed_countries: ["RU"],
+		},
+		line_items: transformedItems,
+		mode: "payment",
+		allow_promotion_codes: true,
+		success_url: `${process.env.NEXTAUTH_URL}/success`,
+		cancel_url: `${process.env.NEXTAUTH_URL}/menu`,
+		metadata: {
+			email,
+			phone,
+			images: JSON.stringify(
+				items.map((item: { image: string }) => item.image)
+			),
+		},
+	});
+	res.status(200).json({ id: session.id });
+};
