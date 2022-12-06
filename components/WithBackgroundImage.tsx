d12ef1089268d00a4b99bb32cf6a1932cf3b8653
neugomonov <neugomonovv@gmail.com>
2022-12-06 20:14:47 +0300

    ✨ index's latter main button motion'ized
    🔨 wrapper applied

--- components/WithBackgroundImage.tsx
+++ components/WithBackgroundImage.tsx
@@ -1,5 +1,4 @@
 import {
-	Box,
 	Button,
 	Flex,
 	Stack,
@@ -8,10 +7,10 @@ import {
 	useColorModeValue,
 	VStack,
 } from "@chakra-ui/react";
-import { motion } from "framer-motion";
 import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
 import index from "../lib";
+import MotionIndexPageButtonBox from "./motion/MotionIndexPageButtonBox";
 
 export default function WithBackgroundImage() {
 	const router = useRouter();
@@ -59,28 +58,7 @@ export default function WithBackgroundImage() {
 						{index.WithBackgroundImage2[lang as "en" | "ru"]}
 					</Text>
 					<Stack direction={"row"}>
-						<Box
-							as={motion.div}
-							drag
-							dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
-							whileDrag={{ scale: 1.2, rotate: 45 }}
-							dragTransition={{ bounceStiffness: 1399, bounceDamping: 10 }}
-							whileTap={{
-								scale: 0.9,
-							}}
-							whileHover={{
-								scale: 1.2,
-								transition: { type: "spring", bounce: 0.8, duration: 1 },
-							}}
-							animate={{
-								scale: [1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1],
-								rotate: [0, 10, -10, 10, -10, 10, -10, 0],
-								transition: {
-									repeat: Infinity,
-									repeatDelay: 5,
-								},
-							}}
-						>
+						<MotionIndexPageButtonBox>
 							<Button
 								colorScheme={"orange"}
 								size="lg"
@@ -93,7 +71,7 @@ export default function WithBackgroundImage() {
 							>
 								{index.WithBackgroundImageButton[lang as "en" | "ru"]}
 							</Button>
-						</Box>
+						</MotionIndexPageButtonBox>
 					</Stack>
 				</Stack>
 			</VStack>
